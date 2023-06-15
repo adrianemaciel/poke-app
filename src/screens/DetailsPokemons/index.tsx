7fc889953059852d0edfa0eca1c4c39b9fde01b3
adrianemaciel <adrianemaciel.developer@gmail.com>
2023-06-15 16:05:09 -0300

    feature: details pokemons

---
+++ src/screens/DetailsPokemons/index.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import {Container} from './styles';
+
+const Details = () => {
+  return (
+    <Container>
+      <Text>Detalhes de pokemons</Text>
+    </Container>
+  );
+};
+
+export default Details;
