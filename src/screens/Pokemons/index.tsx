0e98f776ab0be8e0c3e1174f4242d46d90bfab86
adrianemaciel <adrianemaciel.developer@gmail.com>
2023-06-27 23:05:31 -0300

    consuming API and manipulating pokemon data

--- src/screens/Pokemons/index.tsx
+++ src/screens/Pokemons/index.tsx
@@ -1,45 +1,48 @@
 import {Container} from './styles';
 import {FlatList} from 'react-native';
 import CardPokemon from '../../components/Card';
+import axios from 'axios';
+import {useState, useEffect} from 'react';
 
 type Props = {
   navigation: any;
 };
 
-const DATA = [
-  {
-    id: '01',
-    title: 'Charmeleon',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-  },
-  {
-    id: '02',
-    title: 'Charizard',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-  },
-  {
-    id: '03',
-    title: 'Squirtle',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png',
-  },
-  {
-    id: '04',
-    title: 'Wartortle',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png',
-  },
-  {
-    id: '05',
-    title: 'Charmander',
-    image:
-      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-  },
-];
+type Item = {
+  id: number | string;
+  title: string;
+  image: string;
+  name?: string;
+};
 
 const Pokemons = ({navigation}: Props) => {
+  const [data, setData] = useState([]);
+
+  function PokemonsList() {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=8')
+      .then(function (response) {
+        const pokeList = response.data.results.map(
+          (item: Item, index: number) => {
+            return {
+              id: index,
+              title: item.name,
+              image:
+                'https://p.turbosquid.com/ts-thumb/Y2/L5MN4Z/6BnLhm5o/pokeball_lastrender_thumb/jpg/1310816066/600x600/fit_q87/1fc6afad8246c0aee27246d1b1f35d52b6d6dd6c/pokeball_lastrender_thumb.jpg',
+            };
+          },
+        );
+        setData(pokeList);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    PokemonsList();
+  }, []);
+
   const handlePress = (title: string, image: string) => {
     navigation.navigate('details', {
       itemTitle: title,
@@ -50,15 +53,15 @@ const Pokemons = ({navigation}: Props) => {
   return (
     <Container>
       <FlatList
-        data={DATA}
-        renderItem={({item}) => (
+        data={data}
+        renderItem={({item}: {item: Item}) => (
           <CardPokemon
             onPress={() => handlePress(item.title, item.image)}
             title={item.title}
             image={item.image}
           />
         )}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.id.toString()}
       />
     </Container>
   );
