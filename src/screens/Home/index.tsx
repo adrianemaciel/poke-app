d176b4e464f9dac815c18b9b7af3679fc27841ea
adrianemaciel <adrianemaciel.developer@gmail.com>
2023-12-05 18:14:41 -0300

    perf: svg installation and configuration

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,18 @@
 import {Container, ImageHome, Title} from './styles';
 import LinearGradient from 'react-native-linear-gradient';
+import SGVHOME from '../../../assets/svg/image-home.svg';
 
 const Home = () => {
   return (
     <>
       <Container>
         <LinearGradient
-          colors={['#001373', '#0936A9']}
-          start={{x: 0, y: 1}}
-          end={{x: 1, y: 0}}
+          colors={['#00d4ff', '#020024']}
+          start={{x: 1, y: 0}}
+          end={{x: 0, y: 1}}
           style={{flex: 1}}>
           <Title>Pokedex</Title>
-          <ImageHome
-            source={require('../../../assets/images/image-home.png')}></ImageHome>
+          <SGVHOME width={236} height={307} />
         </LinearGradient>
       </Container>
     </>
